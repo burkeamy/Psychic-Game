bbdb5d8deb83d6d34c038f01937d80ee459cba66
unknown <burkeamy30@gmail.com>
2019-02-25 19:44:01 -0500

    updating and adding an image

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,59 @@
-document.addEventListener('DOMContentLoaded', guessLetter);
+//document.addEventListener('DOMContentLoaded', guessLetter);
 
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9
+var randomLetter;
+var guessed = [];
 
 function guessLetter () {
-    //for (var i = 0; i < 9; i --) {
     
-        var random = Math.floor(Math.random() * (91-65) + 65);
-        var randomLetter = String.fromCharCode(random);
+        var random = Math.floor(Math.random() * (122-97) + 97);
+        randomLetter = String.fromCharCode(random);
+        console.log(randomLetter);
             
         //this is supposed to create a random whole number between 65 and 90; and convert it to a letter using unicode
+}
+guessLetter();
 
-        document.getElementById("kp").addEventListener("keydown", whatLetter);
+document.addEventListener("keydown", whatLetter);
 
-         function whatLetter(key) {
-          var guess = document.getElementById("kp").innerHTML = key;
+function whatLetter(event) {
+    if (guessed.indexOf(event.key.toLowerCase())===-1){
+
+    var guess = event.key.toLowerCase();
+            //var guess = document.getElementById("guessed").innerHTML = key;
+          console.log(guess);
         // this allows the user to input a letter
 
-       var guessed = [];
-            guessed.push[guess];
-            document.write[guess]
+       
+            guessed.push(guess);
+            document.getElementById("guessed").innerHTML = guessed;
+            console.log(guessed);
         //this is supposed to push the guess into an array
          
 
     if (guess === randomLetter) {
-       document.getElementById(win).innerHTML = wins ++;
-       document.getElementById(num).innerHTML = guessesLeft = 9
+        wins++;
+       document.getElementById("wins").innerHTML = ("wins = " + wins);
+       document.getElementById("num").innerHTML = guessesLeft = 9;
+       guessed = []; 
+       guessLetter();
         /*this is supposed to compare the guess from the user to the random creted and, if correct resets the
         guesses to 9 and increases the wins*/
         }
     else if (guessesLeft === 0) {
-        document.getElementById(num).innerHTML = guessesLeft = 9;
-        guessed = []
+        losses++;
+        document.getElementById("lose").innerHTML = ("Losses =  " + losses); 
+        document.getElementById("num").innerHTML = guessesLeft = 9;
+        guessed = [];
+        guessLetter();
         // this is supposed to return the guesses left back to 9 and reset the game
         
         }
     else {
-        document.getElementById(lose).innerHTML = losses ++;
-        document.getElementById(num).innerHTML = guessesLeft --        
+        guessesLeft--;
+        document.getElementById("num").innerHTML = "Guesses  left = " + guessesLeft;       
         }
      //this is supposed to increases losses by one and decreases guesses left by one
     //}
